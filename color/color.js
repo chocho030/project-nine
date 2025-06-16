2ce28920e9e0373e4784aa4dd9ae27fefeb852b7
chocho030 <chocho0003@naver.com>
2025-06-17 00:20:52 +0900

    color 팔레트 추가

--- color/color.js
+++ color/color.js
@@ -3,7 +3,7 @@
 const palette_main = document.getElementById("palette-main");
 const ctx_main = palette_main.getContext("2d");
 
-const wgradient_main = ctx_main.createLinearGradient(0, 0, 500, 0);
+const wgradient_main = ctx_main.createLinearGradient(0, 0, 250, 0);
 
 wgradient_main.addColorStop(0, "white");
 wgradient_main.addColorStop(1, "blue");
@@ -11,33 +11,13 @@ wgradient_main.addColorStop(1, "blue");
 ctx_main.fillStyle = wgradient_main;
 ctx_main.fillRect(0, 0, 500, 500);
 
-const hgradient_main = ctx_main.createLinearGradient(0, 0, 0, 500);
+const hgradient_main = ctx_main.createLinearGradient(0, 0, 0, 250);
 
-hgradient_main.addColorStop(0, "rgba(255,255,255,0 )");
+hgradient_main.addColorStop(0, "rgba(0,0,0,0)");
 hgradient_main.addColorStop(1, "rgba(0,0,0,1)");
 
 ctx_main.fillStyle = hgradient_main;
 ctx_main.fillRect(0, 0, 500, 500);
-
-// 서브 팔레트
-
-const palette_sub = document.getElementById("palette-sub");
-const ctx_sub = palette_sub.getContext("2d");
-
-const wgradient_sub = ctx_sub.createLinearGradient(0, 0, 0, 500);
-
-wgradient_sub.addColorStop(0, "rgb(255, 0, 0)");
-wgradient_sub.addColorStop(0.16, "rgb(255, 255, 0)");
-wgradient_sub.addColorStop(0.32, "rgb(0, 255, 0)");
-wgradient_sub.addColorStop(0.5, "rgb(0, 255, 255)");
-wgradient_sub.addColorStop(0.66, "rgb(0, 0, 255)");
-wgradient_sub.addColorStop(0.82, "rgb(255, 0, 255)");
-wgradient_sub.addColorStop(1, "rgb(255, 0, 0)");
-
-ctx_sub.fillStyle = wgradient_sub;
-ctx_sub.fillRect(0, 0, 20, 500);
-
-// 점 추가. 결과값  ++ 복사
 
 // 점
 const dot_main = document.getElementById("dot-main");
