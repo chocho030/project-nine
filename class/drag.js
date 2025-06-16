f5579d4db7d7c5c25cf5b0bcec7014bc69a5e4fd
chocho030 <chocho0003@naver.com>
2025-06-17 00:16:42 +0900

    수업

--- class/drag.js
+++ class/drag.js
@@ -16,3 +16,25 @@ palette_main.addEventListener("mouseup", (e) => {
 
 클릭 > 마우스 따라감 > 때기
 
+
+원형일때 이벤트 동작
+
+isDrag = false;
+isWhite = false;
+palette_main.addEventListener("mousedown", (e) => { // 사용자가 클릭하면
+  isDrag = true;
+  마우스를 따라가라();
+});
+palette_main.addEventListener("mousemove", (e) => { // 마우스가 이동할때
+  if (!isDrag) return;
+  if(isWhite) return;
+  마우스를 따라가라();
+});
+palette_main.addEventListener("mouseup", (e) => {
+  isDrag = false;
+  마우스를 따라가라();
+});
+
+palette_white.addEventListener("mouseover", (e) => { // 사용자가 클릭하면
+  isWhite = true;
+});
