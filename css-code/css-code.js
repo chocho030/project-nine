67b6795e05d17e94eecfa032dfd1c446ff9e2578
chocho030 <chocho0003@naver.com>
2025-06-21 23:07:37 +0900

    css-code 네비게이션 페이지 전환 구현

--- css-code/css-code.js
+++ css-code/css-code.js
@@ -4,3 +4,15 @@ function copy(num) {
   const text = test[num].innerHTML;
   navigator.clipboard.writeText(text);
 }
+
+function pageCh(num) {
+  const next_page = document.getElementById("content-" + num);
+
+  for (let i = 1; i < 4; i++) {
+    const pages = document.getElementById("content-" + i);
+
+    pages.style.display = "none";
+  }
+
+  next_page.style.display = "block";
+}
