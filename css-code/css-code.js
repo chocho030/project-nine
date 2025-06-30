2111d8c150b6fc6f84253971551a9e20b17bc160
chocho030 <chocho0003@naver.com>
2025-06-30 21:39:46 +0900

    css-code 버튼, 음영 컨텐츠 추가

--- css-code/css-code.js
+++ css-code/css-code.js
@@ -1,14 +1,19 @@
 const test = document.getElementsByClassName("test");
+const shadow = document.getElementsByClassName("shadow");
 
 function copy(num) {
   const text = test[num].innerHTML;
+  navigator.clipboard.writeText(text);
+}
+function copy_shadow(num) {
+  const text = shadow[num].innerHTML;
   navigator.clipboard.writeText(text);
 }
 
 function pageCh(num) {
   const next_page = document.getElementById("content-" + num);
 
-  for (let i = 1; i < 4; i++) {
+  for (let i = 1; i < 3; i++) {
     const pages = document.getElementById("content-" + i);
 
     pages.style.display = "none";
